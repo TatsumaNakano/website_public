34d741fe7c638ac1d7008961404ae2f6b9313027
Tatsuma Nakano <me@tatsuma.co>
2023-08-04 17:54:32 +0900

    chore: Needed to disable PassAllPostsToClient to build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
 
         </body>
         <ConsoleMessage />
-        <PassAllPostsToClient allPostsWP={allPostsFromWP} />
+        {/* <PassAllPostsToClient allPostsWP={allPostsFromWP} /> */}
 
       </HtmlWrapper>
     </RecoilWrapper>
