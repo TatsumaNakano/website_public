255ba839673a7f9336f786d001d4462a685d2942
Tatsuma Nakano <me@tatsuma.co>
2023-08-19 01:56:59 +0900

    chore: Blog sidebar font smaller now. Also some fix on sentence.

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -9,13 +9,13 @@ const SideBar = () => {
                 <h3 className="jp">こんにちは！</h3> */}
 
                 <p className="en">I am a freelance Creative Technologist living in Japan.</p>
-                <p className="jp">フリーでクリエイティブテクノロジストをやっております。</p>
+                <p className="jp">フリーでデザイナー・個人開発者をやっております。</p>
                 <p className="en">My Ikigai is making people happy with what I make.</p>
                 <p className="jp">僕の作った物で人が喜んでくれるのが生き甲斐です。</p>
                 <br />
                 <p className="en">Feel free to drop me a message if you&#39;re interested in</p>
                 <p className="en">working together and bringing our ideas to life.</p>
-                <p className="jp">お仕事のご依頼は右のメッセージ欄から受け付けております。</p>
+                <p className="jp">お仕事のご依頼はお問い合わせから受け付けております。</p>
             </div>
         </div>
     );
